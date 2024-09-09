54febb2c16b143e5782d189336528d9fc6e3a9e7
ashd0wn <nicolas.braudeau@gmail.com>
2024-09-09 07:08:33 +0200

    Update update_latest.sh

--- tools/liquidsoap/update_latest.sh
+++ tools/liquidsoap/update_latest.sh
@@ -4,17 +4,14 @@
 supervisorctl stop all || :
 
 # Get the latest release tag name for Icecast
-tag_name=$(curl -s https://api.github.com/repos/savonet/liquidsoap-release-assets/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
-
 # Construct the release URL for Icecast
-ARCHITECTURE=$(dpkg --print-architecture | awk -F- '{ print $NF }')
-release_url="https://github.com/savonet/liquidsoap-release-assets/releases/download/v2.2.1/liquidsoap_$tag_name-ubuntu-jammy-1_$ARCHITECTURE.deb"
+release_url="https://github.com/savonet/liquidsoap/releases/download/v2.2.3/liquidsoap_2.2.3-ubuntu-jammy-1_amd64.deb"
 
 # Download the latest Liquidsoap .deb package
 curl -LO "$release_url"
 
 # Install Liquidsoap and its dependencies
-dpkg -i liquidsoap_$tag_name-ubuntu-jammy-1_$ARCHITECTURE.deb
+dpkg -i liquidsoap_2.2.3-ubuntu-jammy-1_amd64.deb
 
 # Update package list
 apt_get_with_lock update
@@ -23,7 +20,7 @@ apt_get_with_lock update
 apt_get_with_lock upgrade -y liquidsoap
 
 # Clean up
-rm liquidsoap_$tag_name-ubuntu-jammy-1_$ARCHITECTURE.deb
+rm liquidsoap_2.2.3-ubuntu-jammy-1_amd64.deb
 
 # Start anything
 supervisorctl start all || :
